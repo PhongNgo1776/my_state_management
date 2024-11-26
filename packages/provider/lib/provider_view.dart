560052b2062419180e2c1cf11abc7663939aba8c
PhongNgo <ngothanhphong1776@gmail.com>
2024-11-26 21:30:12 +0700

    Add print for test 3

--- packages/provider/lib/provider_view.dart
+++ packages/provider/lib/provider_view.dart
@@ -39,7 +39,7 @@ class _ProviderViewState<P extends Provider> extends State<ProviderView<P>> {
       CustomProviderManager.removeProvider<P>(tag: widget.tag);
     }
 
-    print('dsdsd');
+    print('12');
     super.dispose();
   }
 }
