980759938424adae3ecab87cb6ad54f5df621110
PhongNgo <ngothanhphong1776@gmail.com>
2024-11-26 20:31:45 +0700

    Update print function

--- packages/provider/lib/provider_view.dart
+++ packages/provider/lib/provider_view.dart
@@ -35,6 +35,8 @@ class _ProviderViewState<P extends Provider> extends State<ProviderView<P>> {
 
   @override
   void dispose() {
+    print('Sort map:' + sortedByValueMap.toString());
+
     if (widget.isAutoDispose) {
       CustomProviderManager.removeProvider<P>(tag: widget.tag);
     }
